43f7b598a3b2ce9bea22b7d90538ef46a0542f14
MrSpaar <62711036+MrSpaar@users.noreply.github.com>
2020-06-24 19:20:18 +0200

    Random related commands

--- cogs/random.py
+++ cogs/random.py
@@ -3,41 +3,29 @@ from discord.ext import commands
 
 from random import choice
 
+
 class random(commands.Cog):
-    
     def __init__(self, bot):
         self.bot = bot
-    
-    @commands.command(brief='!toss [heads/tails]', description='Make a coin toss against the bot')
-    async def pof(self, ctx, arg):
-        piece = choice(['Tails', 'Heads'])
-        if arg.upper() == 'PILE' or arg.upper() == 'TAILS':
-            if arg.upper() == piece.upper():
-                await ctx.send(f':white_check_mark: {piece} ! You won.')
-            else:
-                await ctx.send(f':negative_squared_cross_mark:  {piece} ! You lost.')
-        else:
-            await ctx.send('❌ You must input either "heads" or "tails"')
 
-    @commands.command(brief='!poke [random/nickname]', description='Mention a member (randomly or not)')
-    async def poke(self, ctx, arg):
-        user = ''
-        online_members = []
-        for x in ctx.guild.members:
-            if x.status == discord.Status.online and not x.bot:
-                online_members.append(x)
-            try:
-                if x.name.lower().startswith(arg.lower()) or x.nick.lower().startswith(arg.lower()):
-                    user = x
-            except:
-                pass
-        if arg == 'random':
-            user = choice(online_members).mention
-            await ctx.send(f'Hey {user} !')
-        elif user:
-            await ctx.send(f'Hey {user.mention} !')
+    @commands.command(brief='!pof [pile/face]', description='Toss a coin')
+    async def pof(self, ctx, arg):
+        if arg.lower() == 'head' or arg.lower() == 'tail':
+            piece = choice(['PILE', 'FACE'])
+            if arg.lower() in piece:
+                await ctx.send(f':white_check_mark: {piece}! You won.')
+            else:
+                await ctx.send(f':negative_squared_cross_mark:  {piece}! You lost.')
         else:
-            await ctx.send('❌ You must input either "random" or an online member !')
+            await ctx.send('❌ You must input either "head" or "tail"!')         
+
+    @commands.command(brief='!poke [random/pseudo]', description='Mention a member (randomly or not)')
+    async def poke(self, ctx, arg):
+        members = [x for x in ctx.guild.members if not x.bot]
+        if arg.lower() == 'random':
+            await ctx.send(f'Hey {choice(members).mention} !')
+        else:
+            await ctx.send(f'Hey {arg.mention} !')
 
 def setup(bot):
     bot.add_cog(random(bot))
